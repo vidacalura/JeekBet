1529e06e5d1a17efd7ebf7255e8a2172f4148f7f
php <php__@tutamail.com>
2024-05-21 17:44:47 -0300

    transforma os comandos pra /

--- bot/cmds/jeekbet.py
+++ bot/cmds/jeekbet.py
@@ -1,59 +1,55 @@
+import discord
 from discord.ext import commands
 from discord.ext.commands import has_permissions
-from models.Usuario import Usuario
-from models.Usuario import get_ranking_usuarios
+from discord import app_commands
+from models.Usuario import Usuario, get_ranking_usuarios
+from models.Aposta import Aposta
 
 
-@commands.group()
-async def comandos(ctx):
-    if ctx.invoked_subcommand is None:
-        await ctx.send(f"No, {ctx.subcommand_passed} does not belong to simple")
+class Plebe(app_commands.Group):
+    @app_commands.command()
+    async def salve(self, interaction: discord.Interaction):
+        await interaction.response.send_message(f"Salvee {interaction.user.mention}!")
+
+    @app_commands.command(name="criar-conta",
+                      description="Cadastra um novo usuário no JeekBet")
+    async def criar_conta(self, interaction: discord.Interaction):
+        usu = Usuario(interaction.user.id, interaction.user.name, 0)
+
+        try:
+            usu.cadastrar_usuario()
+            await interaction.response.send_message("Conta cadastrada com sucesso!")
+        except Exception as e:
+            await interaction.response.send_message(str(e))
+
+    @app_commands.command(description="Mostra quanto de dinheiro o usuário tem")
+    async def dinheiro(self, interaction: discord.Interaction):
+        usu = Usuario(interaction.user.id, interaction.user.name, 0)
+
+        try:
+            await interaction.response.send_message(f"Você tem {usu.get_dinheiro()} Jeek points")
+        except Exception as e:
+            await interaction.response.send_message(str(e))
+
+    @app_commands.command(description="Mostra o ranking da Jeeconomia")
+    async def ranking(self, interaction: discord.Interaction):
+        try:
+            i = 1
+            usu_arr = get_ranking_usuarios()
+            for usu in usu_arr:
+                await interaction.response.send_message(f"{i}. {usu.nome} - {usu.dinheiro} Jeek Points")
+                i += 1
+        except Exception as e:
+            await interaction.response.send_message(str(e))
 
 
-@commands.command(description="Responde pong")
-async def ping(ctx):
-    await ctx.send("pong")
-
-@commands.command(name="criar-conta",
-                  description="Cadastra um novo usuário no JeekBet")
-async def criar_conta(ctx):
-    usu = Usuario(ctx.message.author.id, ctx.message.author.name, 0)
-
-    try:
-        usu.cadastrar_usuario()
-        await ctx.send("Conta cadastrada com sucesso!")
-    except Exception as e:
-        await ctx.send(str(e))
-
-@commands.command(description="Mostra quanto de dinheiro o usuário tem")
-async def dinheiro(ctx):
-    usu = Usuario(ctx.message.author.id, ctx.message.author.name, 0)
-
-    try:
-        await ctx.send(f"Você tem {usu.get_dinheiro()} Jeek points")
-    except Exception as e:
-        await ctx.send(str(e))
-
-@commands.command(description="Mostra o ranking da Jeeconomia")
-async def ranking(ctx):
-    try:
-        i = 1
-        usu_arr = get_ranking_usuarios()
-        for usu in usu_arr:
-            await ctx.send(f"{i}. {usu.nome} - {usu.dinheiro} Jeek Points")
-            i += 1
-    except Exception as e:
-        await ctx.send(str(e))
-
-@commands.command(description="Adm teste")
-@has_permissions(administrator=True)
-async def adm(ctx):
-    await ctx.send("Parabéns vc é adm!")
+class Adm(app_commands.Group):
+    @app_commands.command(description="Adm teste")
+    @has_permissions(administrator=True)
+    async def adm(self, interaction: discord.Interaction):
+        await interaction.response.send_message(f"Parabéns {interaction.user.mention} vc é adm!")
 
 
 async def setup(bot):
-    bot.add_command(ping)
-    bot.add_command(criar_conta)
-    bot.add_command(dinheiro)
-    bot.add_command(ranking)
-    bot.add_command(adm)
+    bot.tree.add_command(Adm(name="adm", description="Comandos dos adms"))
+    bot.tree.add_command(Plebe(name="aposta", description="Comandos da plebe"))
